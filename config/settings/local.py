6cf3bafcd4f7e4b2763209c13f5a74934c19975d
Farid Chowdhury <faridstudylu@gmail.com>
2022-11-26 22:44:31 +0600

    Migrate from sqlite3 to postgres

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,17 +1,24 @@
+from decouple import config
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DEVELOPMENT_LOCAL_POSTGRES_DB_NAME'),
+        'USER': config('DEVELOPMENT_LOCAL_POSTGRES_DB_USER'),
+        'PASSWORD': config('DEVELOPMENT_LOCAL_POSTGRES_DB_PASSWORD'),
+        'HOST': config('DEVELOPMENT_LOCAL_POSTGRES_DB_HOST'),
+        'PORT': config('DEVELOPMENT_LOCAL_POSTGRES_DB_PORT'),
     }
 }
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,16 +1,22 @@
 from .base import *
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('PRODUCTION_POSTGRES_DB_NAME'),
+        'USER': config('PRODUCTION_POSTGRES_DB_USER'),
+        'PASSWORD': config('PRODUCTION_POSTGRES_DB_PASSWORD'),
+        'HOST': config('PRODUCTION_POSTGRES_DB_HOST'),
+        'PORT': config('PRODUCTION_POSTGRES_DB_PORT'),
     }
 }
